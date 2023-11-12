2ee4c9b8b7af6ca10862a28646e638af8b503d00
joswigchert <contact@joswigchert.nl>
2023-11-12 12:35:28 +0100

    Add searchbar class

--- AVA/AVA/ui/widgets/searchbar.py
+++ AVA/AVA/ui/widgets/searchbar.py
@@ -1,10 +1,18 @@
 import customtkinter as ctk
 
 
-def search(event=None):
-    query = search_entry.get()
-    # Perform your search or other action here
-    print(f"Searching for: {query}")
+class TextBar(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.entry = ctk.CTkEntry(self)
+        self.entry.pack(side="left", padx=5)
+        self.button = ctk.CTkButton(self, text="Search", command=self.search)
+        self.button.pack(side="right", padx=5)
+
+    def search(event=None):
+        query = search_entry.get()
+        # Perform your search or other action here
+        print(f"Searching for: {query}")
 
 
 root = ctk.CTk()
